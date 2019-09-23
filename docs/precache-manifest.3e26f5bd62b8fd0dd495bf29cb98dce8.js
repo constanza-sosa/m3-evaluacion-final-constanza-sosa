ecbdc34ac7390140ac6c22071781b67cc5c3f4f7
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-09-23 23:11:38 +0200

    Add docs

---
+++ docs/precache-manifest.3e26f5bd62b8fd0dd495bf29cb98dce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be55dddbba7efbffb94199620d5056e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67155b9da8e5b3d0f0ab",
+    "url": "./static/css/main.14173667.chunk.css"
+  },
+  {
+    "revision": "7ae7215d6c0e0d2f6ad6",
+    "url": "./static/js/2.cb8c6842.chunk.js"
+  },
+  {
+    "revision": "67155b9da8e5b3d0f0ab",
+    "url": "./static/js/main.87353ee8.chunk.js"
+  },
+  {
+    "revision": "cff436211b6e0098a568",
+    "url": "./static/js/runtime-main.13f2bb2d.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/LogoRM.75509f2e.png"
+  }
+]);
