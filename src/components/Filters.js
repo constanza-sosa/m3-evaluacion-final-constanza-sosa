d94782c23c42f061c6827fe29cd331fb2fc3a626
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-09-23 18:22:22 +0200

    Add styles to search bar

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -7,8 +7,11 @@ const Filters = props => {
 
   return (
    <div className="search-bar">
-    <label className="search-label">Busca tu personaje favorito</label>
-    <input type="text" className="search-input" onChange={getName} value={query}></input>
+    <p className="search-title">Find your favourite character</p>
+    <div className="sear-bar-name">
+      <label htmlFor="input" className="search-label">By Name: </label>
+      <input id="input" type="text" className="search-input" onChange={getName} value={query} placeholder="Enter a name"></input>
+    </div>
    </div>
   );
 };
